e9c5713914c8ee8170bf73a89ca7f7807ef1a885
Nachiket Patel <nachiket_145@hotmail.com>
2017-06-23 14:40:52 -0400

    Update Magic_Square.R

--- Magic_Square.R
+++ Magic_Square.R
@@ -2,5 +2,5 @@ is_magic <- function(mat){
   n = nrow(mat)
   r1 = apply(mat,1,sum)
   c1 = apply(mat,2,sum)
-  return(sum(r1)==sum(c1) & sum(c1)==sum(diag(mat)*n) & sum(r1)==sum(diag(apply(mat, 2, rev))*n)) 
+  return(sum(r1)==sum(c1) & sum(c1)==sum(diag(mat)*n) & sum(r1)==sum(diag(mat[n:1,])*n)) 
 }

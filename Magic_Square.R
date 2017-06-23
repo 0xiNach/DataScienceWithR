616b14cad7d513ffa49f56da232d431f9782a40e
Nachiket Patel <nachiket_145@hotmail.com>
2017-06-23 13:33:50 -0400

    Add files via upload

---
+++ Magic_Square.R
@@ -0,0 +1,6 @@
+is_magic <- function(mat){
+  n = nrow(mat)
+  r1 = apply(mat,1,sum)
+  c1 = apply(mat,2,sum)
+  return(sum(r1)==sum(c1) & sum(c1)==sum(diag(mat)*n) & sum(r1)==sum(diag(apply(mat, 2, rev))*n)) 
+}
